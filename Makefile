56e4cf72b1accc47df639bd5c7606f0be0d9d785
James A. Rosen <james.a.rosen@gmail.com>
2013-05-18 13:45:10 -0700

    also test against Ember 0.9.8.1 [Resolves #50]

--- Makefile
+++ Makefile
@@ -1,12 +1,22 @@
-all: test
+all: clean test
 
 jshint: npm_install
 	./node_modules/jshint/bin/jshint lib/*.js spec/*Spec.js
 
-test: jshint npm_install
+test_ember_0981: jshint npm_install
+	JQUERY_VERSION=1.7.2 EMBER_VERSION=0.9.8.1 ./spec/buildSuite.js
 	./node_modules/mocha-phantomjs/bin/mocha-phantomjs spec/suite.html
+
+test_ember_10rc3: jshint npm_install
+	JQUERY_VERSION=1.9.1 EMBER_VERSION=1.0.0-rc.3 ./spec/buildSuite.js
+	./node_modules/mocha-phantomjs/bin/mocha-phantomjs spec/suite.html
+
+test: test_ember_0981 test_ember_10rc3
 
 npm_install:
 	npm install
 
-.PHONY: jshint test npm_install
+clean:
+	rm -f spec/suite.html
+
+.PHONY: jshint test test_ember_0981 test_ember_10rc3 npm_install clean
---
+++ spec/buildSuite.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var fs   = require('fs'),
+    hdbs = require('handlebars').create();
+
+function versionFor(library) {
+  var envPropertyName = library.toUpperCase() + '_VERSION';
+      version = process.env[ envPropertyName ];
+
+  if (version == null || version === '') {
+    console.error('Must set environment variable ' + envPropertyName);
+    process.exit(1);
+  }
+
+  var path = 'vendor/' + library + '-' + version + '.js';
+
+  if (!fs.existsSync(path)) {
+    console.error('No such file: ' + path);
+    process.exit(1);
+  }
+
+  return version;
+}
+
+var jQueryVersion    = versionFor('jquery'),
+    emberVersion     = versionFor('ember');
+    template         = fs.readFileSync('spec/suite.hdbs').toString(),
+    compiledTemplate = hdbs.compile(template),
+    templateData     = { jQueryVersion: jQueryVersion, emberVersion: emberVersion },
+    outputPath       = 'spec/suite.html';
+
+fs.writeFileSync(outputPath, compiledTemplate(templateData));
+console.info('Wrote suite to ' + outputPath);
