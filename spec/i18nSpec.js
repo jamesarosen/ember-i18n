4405427d653e9cf0e2c79869e9664537b070787c
James A. Rosen <james@zendesk.com>
2014-07-05 16:43:54 -0700

    extract common spec setup/teardown to spec_support.js

--- spec/i18nSpec.js
+++ spec/i18nSpec.js
@@ -12,33 +12,8 @@
       });
     }
 
-    beforeEach(function() {
-      this.originalTranslations = Ember.I18n.translations;
-
-      Ember.I18n.translations = {
-        'foo.bar': 'A Foobar',
-        'foo.bar.named': 'A Foobar named {{name}}',
-        'foo.save.disabled': 'Saving Foo...',
-        'foos.zero': 'No Foos',
-        'foos.one': 'One Foo',
-        'foos.other': 'All {{count}} Foos',
-        'bars.all': 'All {{count}} Bars',
-        baz: {
-          qux: 'A qux appears'
-        },
-        fum: {
-          one: 'A fum',
-          other: '{{count}} fums'
-        }
-      };
-
-      CLDR.defaultLanguage = 'ksh';
-    });
-
     afterEach(function() {
       if (view != null) view.destroy();
-      Ember.I18n.translations = this.originalTranslations;
-      CLDR.defaultLanguage = null;
     });
 
     it('exists', function() {
--- spec/spec_support.js
+++ spec/spec_support.js
@@ -5,4 +5,32 @@
     mocha.globals([ 'jQuery*' ]);
   }
 
+  beforeEach(function() {
+    this.originalTranslations = Ember.I18n.translations;
+
+    Ember.I18n.translations = {
+      'foo.bar': 'A Foobar',
+      'foo.bar.named': 'A Foobar named {{name}}',
+      'foo.save.disabled': 'Saving Foo...',
+      'foos.zero': 'No Foos',
+      'foos.one': 'One Foo',
+      'foos.other': 'All {{count}} Foos',
+      'bars.all': 'All {{count}} Bars',
+      baz: {
+        qux: 'A qux appears'
+      },
+      fum: {
+        one: 'A fum',
+        other: '{{count}} fums'
+      }
+    };
+
+    CLDR.defaultLanguage = 'ksh';
+  });
+
+  afterEach(function() {
+    Ember.I18n.translations = this.originalTranslations;
+    CLDR.defaultLanguage = null;
+  });
+
 }());
