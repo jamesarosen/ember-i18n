729f8bd0105f94edc4dc26b43274c777c8c4d136
Matt Nguyen <matt@checkmate.io>
2015-03-05 12:43:43 -0800

    Schedule setting up observers in afterRender queue
    https://github.com/jamesarosen/ember-i18n/issues/198
    
    When including TranslateableProperties in ObjectControllers, setting
    properties to their translations fail because the model (ie content)
    isn't available. This change schedules this work in the afterRender queue so
    that models are set on the controller.

--- spec/translateablePropertiesSpec.js
+++ spec/translateablePropertiesSpec.js
@@ -1,16 +1,14 @@
 describe('TranslateableProperties', function() {
 
   it('translates ___Translation attributes on the object', function() {
-    var subject = Ember.Object.extend(Ember.I18n.TranslateableProperties).create({
-      titleTranslation: 'foo.bar'
-    });
+    var klass = Ember.Object.extend(Ember.I18n.TranslateableProperties);
+    var subject = Ember.run(klass, 'create', { titleTranslation: 'foo.bar' });
     expect(subject.get('title')).to.equal('A Foobar');
   });
 
   it('updates translations when the upstream value changes', function() {
-    var subject = Ember.Object.extend(Ember.I18n.TranslateableProperties).create({
-      titleTranslation: 'foo.bar'
-    });
+    var klass = Ember.Object.extend(Ember.I18n.TranslateableProperties);
+    var subject = Ember.run(klass, 'create', { titleTranslation: 'foo.bar' });
     expect(subject.get('title')).to.equal('A Foobar');
     subject.set('titleTranslation', 'foos.zero');
     expect(subject.get('title')).to.equal('No Foos');
