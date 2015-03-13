a1886287c937a29125dd6e8e988fa6eeb8dd876d
Alex Grant <alex@vermonster.com>
2015-03-13 14:16:29 -0400

    Reset the missingMessage after tests that change it

--- spec/tSpec.js
+++ spec/tSpec.js
@@ -59,17 +59,27 @@ describe('Ember.I18n.t', function() {
     expect(Ember.I18n.t('nothing.here')).to.equal('Missing translation: nothing.here');
   });
 
-  it('warns with a custom message', function() {
-    Ember.I18n.missingMessage = function(key) { return "there.is." + key + ".to.see"; };
-    expect(Ember.I18n.t('nothing.here')).to.equal('there.is.nothing.here.to.see');
-  });
+  describe('missing message', function(){
+    beforeEach(function() {
+      this.oldMissingMessage = Ember.I18n.missingMessage;
+    });
 
-  it('warns with a custom message that includes the passed context', function() {
-    Ember.I18n.missingMessage = function(key, context) {
-      var values = Object.keys(context).map(function(key) { return context[key]; });
-      return key + ',' + (values.join(','));
-    };
-    expect(Ember.I18n.t('foo', { arg1: 'bar', arg2: 'qux' })).to.equal('foo,bar,qux');
+    afterEach(function() {
+      Ember.I18n.missingMessage = this.oldMissingMessage;
+    });
+
+    it('can be set to a custom message', function() {
+      Ember.I18n.missingMessage = function(key) { return "there.is." + key + ".to.see"; };
+      expect(Ember.I18n.t('nothing.here')).to.equal('there.is.nothing.here.to.see');
+    });
+
+    it('can make use of the passed context', function() {
+      Ember.I18n.missingMessage = function(key, context) {
+        var values = Object.keys(context).map(function(key) { return context[key]; });
+        return key + ',' + (values.join(','));
+      };
+      expect(Ember.I18n.t('foo', { arg1: 'bar', arg2: 'qux' })).to.equal('foo,bar,qux');
+    });
   });
 
   describe('missing event', function() {
