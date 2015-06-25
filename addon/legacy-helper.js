684bbfbc29352edda54111818490a7475f8252d9
danshapir <s.dan@live.com>
2015-06-25 20:38:40 +0300

    legacy helper: destroy stream when view is destroyed
    Closes #253
    Closes #258
    Closes #259

--- addon/legacy-helper.js
+++ addon/legacy-helper.js
@@ -15,6 +15,11 @@ if (Ember.Helper == null) {
       return value === undefined ? '' : i18n.t(value, readHash(hash));
     });
 
+    // Once the view is destroyed destroy the steam as well
+    env.data.view.one('willDestroyElement', out, function() {
+      this.destroy();
+    });
+
     // observe any hash arguments that are streams:
     Ember.keys(hash).forEach(function(key) {
       const value = hash[key];
