673f5e9507f37e271193ae1f36412f8936eb53ce
Kelly Selden <kellyselden@gmail.com>
2015-07-28 10:48:16 -0400

    adds a runtime locale to the array of locales on disk

--- tests/unit/utils/locale-test.js
+++ tests/unit/utils/locale-test.js
@@ -4,16 +4,51 @@ moduleFor('service:i18n', 'I18nService#locales', {
   integration: true
 });
 
-const locales = [
-  'en-bw',
-  'en-ps',
-  'en-wz',
-  'en',
-  'es'
-];
-
 test('locales have the correct length and names', function(assert) {
+  const locales = [
+    'en-bw',
+    'en-ps',
+    'en-wz',
+    'en',
+    'es'
+  ];
+
   const i18n = this.subject();
+
+  assert.deepEqual(i18n.get('locales'), locales);
+});
+
+test('addTranslations adds to locales', function(assert) {
+  const locales = [
+    'en-bw',
+    'en-ps',
+    'en-wz',
+    'en',
+    'es',
+    'it'
+  ];
+
+  const i18n = this.subject({ locale: 'en' });
+
+  i18n.addTranslations('it', {});
+
+  assert.deepEqual(i18n.get('locales'), locales);
+});
+
+test('addTranslations doesn\'t add duplicate locales', function(assert) {
+  const locales = [
+    'en-bw',
+    'en-ps',
+    'en-wz',
+    'en',
+    'es',
+    'it'
+  ];
+
+  const i18n = this.subject({ locale: 'en' });
+
+  i18n.addTranslations('it', {});
+  i18n.addTranslations('it', {});
 
   assert.deepEqual(i18n.get('locales'), locales);
 });
