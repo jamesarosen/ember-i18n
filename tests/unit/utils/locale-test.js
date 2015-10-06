f98b375e38f9e9c23143b05abe8b857f0c15a6be
James A. Rosen <james@fastly.com>
2015-10-06 13:10:33 -0700

    sort I18nService#locales
    Tests were failing because the set of locales was correct, but wrongly
    ordered. Ordering the list of locales is a sensible thing to do and not
    terribly expensive.

--- tests/unit/utils/locale-test.js
+++ tests/unit/utils/locale-test.js
@@ -5,50 +5,49 @@ moduleFor('service:i18n', 'I18nService#locales', {
 });
 
 test('locales have the correct length and names', function(assert) {
-  const locales = [
+  const expected = [
+    'en',
     'en-bw',
     'en-ps',
     'en-wz',
-    'en',
     'es'
   ];
+  const actual = this.subject().get('locales');
 
-  const i18n = this.subject();
-
-  assert.deepEqual(i18n.get('locales'), locales);
+  assert.deepEqual(actual, expected, JSON.stringify(actual));
 });
 
 test('addTranslations adds to locales', function(assert) {
-  const locales = [
+  const expected = [
+    'en',
     'en-bw',
     'en-ps',
     'en-wz',
-    'en',
     'es',
     'it'
   ];
 
   const i18n = this.subject({ locale: 'en' });
-
   i18n.addTranslations('it', {});
+  const actual = i18n.get('locales');
 
-  assert.deepEqual(i18n.get('locales'), locales);
+  assert.deepEqual(actual, expected, JSON.stringify(actual));
 });
 
 test('addTranslations doesn\'t add duplicate locales', function(assert) {
-  const locales = [
+  const expected = [
+    'en',
     'en-bw',
     'en-ps',
     'en-wz',
-    'en',
     'es',
     'it'
   ];
 
   const i18n = this.subject({ locale: 'en' });
-
   i18n.addTranslations('it', {});
   i18n.addTranslations('it', {});
+  const actual = i18n.get('locales');
 
-  assert.deepEqual(i18n.get('locales'), locales);
+  assert.deepEqual(actual, expected, JSON.stringify(actual));
 });
