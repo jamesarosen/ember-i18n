7ee73d4dbe2e1911f1d2f0faea9efaf746405743
Ofri Dagan <ofridagan@gmail.com>
2015-10-09 22:14:32 +0300

    Call missingMessage on the service:i18n
    Closes #322
    Closes #323

--- addon/utils/locale.js
+++ addon/utils/locale.js
@@ -88,10 +88,11 @@ export default class Locale {
   }
 
   _defineMissingTranslationTemplate(key) {
+    const i18n = this.container.lookup('service:i18n');
     const missingMessage = this.container.lookupFactory('util:i18n/missing-message');
     const locale = this.id;
 
-    function missingTranslation(data) { return missingMessage(locale, key, data); }
+    function missingTranslation(data) { return missingMessage.call(i18n, locale, key, data); }
 
     missingTranslation._isMissing = true;
     this.translations[key] = missingTranslation;
