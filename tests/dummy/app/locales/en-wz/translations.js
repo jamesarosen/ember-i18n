54db6e2bfa47a1d082e4dfb72cf579f2bba46690
Šuška Boris <boris.suska@ysoft.com>
2016-07-14 14:19:40 +0200

    Test for issue #332 and #383 added.
    Tests if count is not passed or is undefined or null, 'pluralized.undefined-count' translation is used.

--- tests/dummy/app/locales/en-wz/translations.js
+++ tests/dummy/app/locales/en-wz/translations.js
@@ -8,5 +8,10 @@ export default {
   'pluralized.short-translation': {
     one: 'One Click',
     other: '{{count}} Clicks'
-  }
+  },
+
+  'pluralized.undefined-count': 'No clicks number provided',
+  'pluralized.undefined-count.zero': 'Zero Clicks',
+  'pluralized.undefined-count.one': 'One Click',
+  'pluralized.undefined-count.other': '{{count}} Clicks'
 };
