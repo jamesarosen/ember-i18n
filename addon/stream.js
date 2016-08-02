7aa2e8cf753375486b9f92a4a1c4b20d90c32658
Wang Zhongliang <wangz@garena.com>
2016-08-02 16:41:01 +0800

    Add Ember alpha version support

--- addon/stream.js
+++ addon/stream.js
@@ -8,9 +8,23 @@ import Ember from 'ember';
 // See https://github.com/dockyard/ember-cli-i18n/blob/v0.0.6/addon/utils/stream.js
 //
 // As of v2.7, Streams are moved to `ember-htmlbars`, we need to check if `ember-metal/streams/stream` exists
+// As of v2.9-alpha.1 (Glimmer 2), Streams are not available.
 
 const _registry = Ember.__loader.registry;
 const _require = Ember.__loader.require;
 
-export default _registry['ember-metal/streams/stream'] ? _require('ember-metal/streams/stream')['default'] : _require('ember-htmlbars/streams/stream')['default'];
-export const readHash = _registry['ember-metal/streams/utils'] ? _require('ember-metal/streams/utils').readHash :  _require('ember-htmlbars/streams/utils').readHash;
+const _metalStream = _registry['ember-metal/streams/stream'];
+const _htmlbarsStream = _registry['ember-htmlbars/streams/stream'];
+
+let stream;
+
+export let readHash;
+const _isStreamAvailable = _metalStream || _htmlbarsStream;
+
+if (_isStreamAvailable) {
+  stream = _metalStream ? _require('ember-metal/streams/stream')['default'] : _require('ember-htmlbars/streams/stream')['default'];
+  readHash = _registry['ember-metal/streams/utils'] ? _require('ember-metal/streams/utils').readHash : _require('ember-htmlbars/streams/utils').readHash;
+}
+
+export default stream;
+
