be9d6885539b12333d43d5150f3ffb2d9d4e84e0
James A. Rosen <james@fastly.com>
2016-08-26 12:17:38 -0700

    Include ember 1.12 support only on 1.12
    Use ember-cli-version-checker and broccoli-funnel to selectively include
    legacy- or non-legacy code in the build depending on the Ember version.
    For Ember 1.12.x, include `addon/legacy/*` and
    `app/initializers/ember-i18n-legacy-helper.js`. For Ember 1.13+, include
    `addon/helper.js` and `app/helpers/t.js`.
    
     * `addon/helper.js`, the Ember 1.13+ version, can rely on `Ember.Helper`
       existing
     * `addon/legacy-helper.js` -> `addon/legacy/helper.js`
     * `addon/stream.js` -> `addon/legacy/stream.js`
     * `app/instance-initializers/ember-i18n.js` -> `addon/legacy/initializer.js`,
       exported to `app/initializers/ember-i18n-legacy-helper.js`
     * remove `app/initializers/ember-i18n.js` as Ember 1.13+ doesn't require
       any custom initialization

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 /* jshint node: true */
 'use strict';
+var Funnel = require('broccoli-funnel');
+var VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'ember-i18n',
+  isLocalizationFramework: true,
 
-  isLocalizationFramework: true
+  config: function() {
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember', 'bower');
+    this.hasEmberHelper = dep.satisfies('>= 1.13.0');
+    return this._super.config.apply(this, arguments);
+  },
+
+  treeFor: function(name) {
+    var result = this._super.treeFor.apply(this, arguments);
+
+    if (this.hasEmberHelper) {
+      result = exclude(result, [
+        'initializers/ember-i18n-legacy-helper.js',
+        'modules/ember-i18n/legacy/helper.js',
+        'modules/ember-i18n/legacy/initializer.js',
+        'modules/ember-i18n/legacy/stream.js'
+      ]);
+    } else {
+      result = exclude(result, [
+        'helpers/t.js',
+        'modules/ember-i18n/helper.js'
+      ]);
+    }
+
+    return result;
+  }
 };
+
+function exclude(tree, files) {
+  return new Funnel(tree, {
+    exclude: files
+  });
+}
