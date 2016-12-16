0a253fcae493bce5a52685c16db8ec714082b2f0
k-fish <k-fish@users.noreply.github.com>
2016-12-16 15:40:04 -0500

    Update helper to allow binding hash to be passed
    A second parameter (non hash) should be able to be set to cover cases when a hash has been built and needs to be passed to the `{{t}}` helper.

--- addon/helper.js
+++ addon/helper.js
@@ -1,15 +1,18 @@
 import Ember from "ember";
 
-export default Ember.Helper.extend({
-  i18n: Ember.inject.service(),
+const { assign, get, inject, Helper, observer } = Ember;
 
-  compute: function(params, interpolations) {
-    const key = params[0];
-    const i18n = this.get('i18n');
+export default Ember.Helper.extend({
+  i18n: inject.service(),
+
+  compute([key, interpolationHash = {}, ...rest], interpolations) {
+    assign(interpolations, interpolationHash);
+    
+    const i18n = get(this, 'i18n');
     return i18n.t(key, interpolations);
   },
 
-  _recomputeOnLocaleChange: Ember.observer('i18n.locale', function() {
+  _recomputeOnLocaleChange: observer('i18n.locale', function() {
     this.recompute();
   })
 });
